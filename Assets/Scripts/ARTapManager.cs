6f42deb5c9182443cd0e9d5aef70593f2271050a
ANNDo4320 <ngoc.do5@gmail.com>
2018-10-21 12:53:55 +1100

    Upload of project files
    This is the Unity project files for the app

---
+++ Assets/Scripts/ARTapManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ARTapManager : MonoBehaviour {
+    public GameObject EquipmentPanel;
+    public TextMeshProUGUI PanelName;
+    public TextMeshProUGUI PanelWeight;
+    public TextMeshProUGUI PanelDescription;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Debug.Log("Something Hit");
+                if (raycastHit.collider.name == "Soccer")
+                {
+                    Debug.Log("Soccer Ball clicked");
+                }
+
+                //OR with Tag
+
+                if (raycastHit.collider.CompareTag("Equipment"))
+                {
+                    EquipmentPanel.SetActive(true);
+                    PanelName.text = raycastHit.collider.gameObject.GetComponent<EquipmentScript>().EquName;
+                    PanelWeight.text = raycastHit.collider.gameObject.GetComponent<EquipmentScript>().EquWeight;
+                    PanelDescription.text = raycastHit.collider.gameObject.GetComponent<EquipmentScript>().EquDescription;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SubMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class SubMenuManager : MonoBehaviour {
+    public GameObject Submenu;
+    public Image ButtonImage;
+    public Sprite Down;
+    public Sprite Up;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	public void ClickedSubMenuButton()
+    {
+        if(Submenu.activeSelf == false)
+        {
+            Submenu.SetActive(true);
+            ButtonImage.sprite = Down;
+        }
+        else
+        {
+            Submenu.SetActive(false);
+            ButtonImage.sprite = Up;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
